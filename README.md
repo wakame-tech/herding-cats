dd15a43a65b9bde5aa82ac270023ef00f3a7aff6
wakame-tech <kamata1919@gmail.com>
2020-10-26 00:51:55 +0900

    add: README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# herding-cats
+[猫番](https://eed3si9n.com/herding-cats/ja/) 学習記録
+
+## scrapbox
+https://scrapbox.io/wakame-memorundum/Herding_Cats
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,5 +4,9 @@ import cats._, cats.data._, cats.implicits._
 
 object Hello {
   def main(args: Array[String]) {
+    val f = {(_: Int) * 3}
+    val g = {(_: Int) + 1}
+    val x: Either[String, Int] = Right(1)
+    println((x map (f map g)) === (x map f map g))
   }
 }
